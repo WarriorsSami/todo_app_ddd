de4e984975eade2bb401c16b5b796b1cb1e5019e
WarriorsSami <sbarbutdica@gmail.com>
2021-12-20 14:11:15 +0200

    feat(domain): add auth facade and related failures

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:todo_app_ddd/domain/auth/email_address.dart';
+import 'package:todo_app_ddd/domain/auth/password.dart';
+import 'package:todo_app_ddd/domain/auth/auth_failures.dart';
+
+typedef AFailOrVal = Either<AuthFailure, Unit>;
+
+abstract class IAuthFacade {
+  Future<AFailOrVal> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<AFailOrVal> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<AFailOrVal> signInWithGoogle();
+}
