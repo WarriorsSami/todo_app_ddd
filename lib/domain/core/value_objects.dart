46eb56f1c22b541f547b75bc1430540293b43fa2
WarriorsSami <sbarbutdica@gmail.com>
2022-01-08 20:42:27 +0200

    feat: define unique id vvo

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -2,22 +2,24 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:todo_app_ddd/domain/core/errors.dart';
 import 'package:todo_app_ddd/domain/core/value_failures.dart';
+import 'package:uuid/uuid.dart';
 
 typedef FailOrVal<T> = Either<ValueFailure<T>, T>;
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
+
   FailOrVal<T> get value;
 
   bool isValid() => value.isRight();
 
   /// Throws [UnexpectedValueError] containing the [ValueFailure]
   T getOrCrash() => value.fold(
-    (f) => throw UnexpectedValueError(f),
-    // id = identity or the same as (right) => right
-    id,
-  );
+        (f) => throw UnexpectedValueError(f),
+        // id = identity or the same as (right) => right
+        id,
+      );
 
   @override
   String toString() => 'Value($value)';
@@ -25,10 +27,31 @@ abstract class ValueObject<T> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is ValueObject<T> &&
-              runtimeType == other.runtimeType &&
-              value == other.value;
+      other is ValueObject<T> &&
+          runtimeType == other.runtimeType &&
+          value == other.value;
 
   @override
   int get hashCode => value.hashCode;
+}
+
+class UniqueId extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory UniqueId() {
+    return UniqueId._(
+      right(const Uuid().v1()),
+    );
+  }
+
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    return UniqueId._(
+      right(
+        uniqueId,
+      ),
+    );
+  }
+
+  const UniqueId._(this.value);
 }
