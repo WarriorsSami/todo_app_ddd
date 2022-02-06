2437a8864b99cf07a284b55aacf86ed0500eafdf
WarriorsSami <sbarbutdica@gmail.com>
2022-02-06 23:27:16 +0200

    feat(notes): create value objects
    Define validators for the corresponding notes' value failures and value objects for the note body, the todo name, the color and the list of 3 todo items

---
+++ lib/domain/notes/value_objects.dart
@@ -0,0 +1,115 @@
+import 'dart:ui';
+
+import 'package:dartz/dartz.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:todo_app_ddd/domain/core/value_objects.dart';
+import 'package:todo_app_ddd/domain/core/value_transformers.dart';
+import 'package:todo_app_ddd/domain/core/value_validators.dart';
+
+class NoteBody extends ValueObject<String> {
+  @override
+  final FailOrVal<String> value;
+
+  static const maxLength = 1000;
+
+  factory NoteBody(String input) {
+    return NoteBody._(
+      validateMaxStringLength(input, maxLength).flatMap(validateStringNonEmpty),
+    );
+  }
+
+  const NoteBody._(this.value);
+}
+
+class TodoName extends ValueObject<String> {
+  @override
+  final FailOrVal<String> value;
+
+  static const maxLength = 30;
+
+  factory TodoName(String input) {
+    return TodoName._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(validateStringNonEmpty)
+          .flatMap(validateSingleLine),
+    );
+  }
+
+  const TodoName._(this.value);
+}
+
+class NoteColor extends ValueObject<Color> {
+  static const List<Color> predefinedColors = [
+    Color(0xfffafafa), // canvas
+    Color(0xfffa8072), // salmon
+    Color(0xfffedc56), // mustard
+    Color(0xffd0f0c0), // tea
+    Color(0xfffca3b7), // flamingo
+    Color(0xff997950), // tortilla
+    Color(0xfffffdd0), // cream
+    Color(0xffff0000), // red
+    Color(0xff00bfff), // deep sky blue
+    Color(0xff87ceeb), // sky blue
+    Color(0xff7fff00), // lime
+    Color(0xff00fa9a), // medium spring green
+    Color(0xffcd5c5c), // indian red
+    Color(0xfffffac8), // ivory
+    Color(0xff800000), // maroon
+    Color(0xff66cdaa), // turquoise
+    Color(0xff000080), // navy
+    Color(0xff808000), // olive
+    Color(0xff800080), // purple
+    Color(0xffff00ff), // fuchsia
+    Color(0xff00ff00), // green
+    Color(0xff008080), // teal
+    Color(0xfffaebd7), // bisque
+    Color(0xffcd853f), // peru
+    Color(0xff708090), // slate gray
+    Color(0xff4682b4), // steel blue
+    Color(0xffd2b48c), // tan
+    Color(0xff006400), // dark green
+    Color(0xff483d8b), // slate blue
+    Color(0xff2f4f4f), // dim gray
+    Color(0xff2e8b57), // sea green
+    Color(0xff228b22), // forest green
+    Color(0xff00ced1), // dark turquoise
+    Color(0xff9400d3), // dark violet
+    Color(0xff00bfff), // deep sky blue
+    Color(0xff1e90ff), // dodger blue
+    Color(0xff00ffff), // cyan
+  ];
+
+  @override
+  final FailOrVal<Color> value;
+
+  factory NoteColor(Color input) {
+    return NoteColor._(
+      right(makeColorOpaque(input)),
+    );
+  }
+
+  const NoteColor._(this.value);
+}
+
+class List3<T> extends ValueObject<KtList<T>> {
+  @override
+  final FailOrVal<KtList<T>> value;
+
+  static const maxLength = 3;
+
+  factory List3(KtList<T> input) {
+    return List3<T>._(
+      validateMaxListLength(input, maxLength),
+    );
+  }
+
+  const List3._(this.value);
+
+  int get length {
+    return value.getOrElse(() => emptyList()).size;
+  }
+
+  bool get isFull {
+    return length == maxLength;
+  }
+}
