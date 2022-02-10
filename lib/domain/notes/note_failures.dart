941d025606daebddcd9ccb85dbd5fef3148033d9
WarriorsSami <sbarbutdica@gmail.com>
2022-02-10 20:20:56 +0200

    feat: define note repository interface and note failures

---
+++ lib/domain/notes/note_failures.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'note_failures.freezed.dart';
+
+typedef NoteFailureOr<T> = Either<NoteFailure, T>;
+
+@freezed
+class NoteFailure with _$NoteFailure {
+  const factory NoteFailure.unexpected() = _Unexpected;
+}
